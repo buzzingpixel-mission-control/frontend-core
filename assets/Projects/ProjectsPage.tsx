89ce1f4e9456284cdca5c1cf1069ecb535eba68a
TJ Draper <tj@buzzingpixel.com>
2023-04-26 10:05:38 -0500

    Add filtering to projects

--- assets/Projects/ProjectsPage.tsx
+++ assets/Projects/ProjectsPage.tsx
@@ -28,6 +28,11 @@ const ProjectsPage = () => {
     setPageName('Projects');
 
     const [
+        filterText,
+        setFilterText,
+    ] = useState<string>('');
+
+    const [
         addProjectIsOpen,
         setAddProjectIsOpen,
     ] = useState<boolean>(false);
@@ -52,7 +57,9 @@ const ProjectsPage = () => {
         return null;
     };
 
-    if (data.length < 1) {
+    let projects = transformProjects(data);
+
+    if (projects.length < 1) {
         return <>
             {portals()}
             <ProjectTabs />
@@ -67,7 +74,25 @@ const ProjectsPage = () => {
         </>;
     }
 
-    const projects = transformProjects(data);
+    const FilterInput = <div className="mb-4">
+        <input
+            type="text"
+            name="filter"
+            id="filter"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+            placeholder="Filter results"
+            value={filterText}
+            onChange={(e) => {
+                setFilterText(e.target.value);
+            }}
+        />
+    </div>;
+
+    if (filterText !== '') {
+        projects = projects.filter((project) => project.title.toLowerCase().indexOf(filterText.toLowerCase()) > -1
+            || project.slug.toLowerCase().indexOf(filterText.toLowerCase()) > -1
+            || project.description.toLowerCase().indexOf(filterText.toLowerCase()) > -1);
+    }
 
     return <>
         {portals()}
@@ -86,87 +111,97 @@ const ProjectsPage = () => {
                     </button>
                 </div>
             </div>
-            <div className="bg-white rounded-xl px-4">
-                <ul role="list" className="divide-y divide-gray-100">
-                    {projects.map((project) => (
-                        <li key={project.id} className="flex items-center justify-between gap-x-6 py-5">
-                            <div className="min-w-0">
-                                <div className="flex items-start gap-x-3">
-                                    <p className="text-sm font-semibold leading-6 text-gray-900">{project.title}</p>
-                                    <p
-                                        className={classNames(
-                                            statuses[project.status.toString()],
-                                            'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset',
-                                        )}
-                                    >
-                                        {project.status.toString()}
-                                    </p>
+            {FilterInput}
+            {(() => {
+                if (projects.length < 1) {
+                    return <NoResultsAddItem
+                        icon={<ClipboardDocumentListIcon />}
+                        headline="No projects match your filters"
+                    />;
+                }
+
+                return <div className="bg-white rounded-xl px-4">
+                    <ul role="list" className="divide-y divide-gray-100">
+                        {projects.map((project) => (
+                            <li key={project.id} className="flex items-center justify-between gap-x-6 py-5">
+                                <div className="min-w-0">
+                                    <div className="flex items-start gap-x-3">
+                                        <p className="text-sm font-semibold leading-6 text-gray-900">{project.title}</p>
+                                        <p
+                                            className={classNames(
+                                                statuses[project.status.toString()],
+                                                'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset',
+                                            )}
+                                        >
+                                            {project.status.toString()}
+                                        </p>
+                                    </div>
+                                    <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                                        <p className="whitespace-nowrap">
+                                            {project.slug}
+                                        </p>
+                                        <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                                            <circle cx={1} cy={1} r={1}/>
+                                        </svg>
+                                        <p className="truncate">Created {project.createdAtDate.toLocaleDateString()}</p>
+                                    </div>
                                 </div>
-                                <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
-                                    <p className="whitespace-nowrap">
-                                        {project.slug}
-                                    </p>
-                                    <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
-                                        <circle cx={1} cy={1} r={1}/>
-                                    </svg>
-                                    <p className="truncate">Created {project.createdAtDate.toLocaleDateString()}</p>
-                                </div>
-                            </div>
-                            <div className="flex flex-none items-center gap-x-4">
-                                <Link
-                                    to={project.href}
-                                    className="block rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                >
-                                    View project<span className="sr-only">, {project.title}</span>
-                                </Link>
-                                <Menu as="div" className="relative flex-none">
-                                    <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                                        <span className="sr-only">Open options</span>
-                                        <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true"/>
-                                    </Menu.Button>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
+                                <div className="flex flex-none items-center gap-x-4">
+                                    <Link
+                                        to={project.href}
+                                        className="block rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                                     >
-                                        <Menu.Items
-                                            className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <span
-                                                        className={classNames(
-                                                            active ? 'bg-gray-50' : '',
-                                                            'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900',
-                                                        )}
-                                                    >
+                                        View project<span className="sr-only">, {project.title}</span>
+                                    </Link>
+                                    <Menu as="div" className="relative flex-none">
+                                        <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                                            <span className="sr-only">Open options</span>
+                                            <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true"/>
+                                        </Menu.Button>
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-100"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                            <Menu.Items
+                                                className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <span
+                                                            className={classNames(
+                                                                active ? 'bg-gray-50' : '',
+                                                                'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900',
+                                                            )}
+                                                        >
                                                         Edit<span className="sr-only">, {project.title}</span>
                                                     </span>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <span
-                                                        className={classNames(
-                                                            active ? 'bg-gray-50' : '',
-                                                            'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900',
-                                                        )}
-                                                    >
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <span
+                                                            className={classNames(
+                                                                active ? 'bg-gray-50' : '',
+                                                                'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900',
+                                                            )}
+                                                        >
                                                         Archive<span className="sr-only">, {project.title}</span>
                                                     </span>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                                                    )}
+                                                </Menu.Item>
+                                            </Menu.Items>
+                                        </Transition>
+                                    </Menu>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>;
+            })()}
         </div>
     </>;
 };
--- dist/Projects/ProjectsPage.js
+++ dist/Projects/ProjectsPage.js
@@ -51,8 +51,9 @@ function classNames() {
 }
 var ProjectsPage = function () {
     (0, setPageName_1.default)('Projects');
-    var _a = (0, react_1.useState)(false), addProjectIsOpen = _a[0], setAddProjectIsOpen = _a[1];
-    var _b = (0, ProjectsData_1.useProjectsData)(), status = _b.status, data = _b.data;
+    var _a = (0, react_1.useState)(''), filterText = _a[0], setFilterText = _a[1];
+    var _b = (0, react_1.useState)(false), addProjectIsOpen = _b[0], setAddProjectIsOpen = _b[1];
+    var _c = (0, ProjectsData_1.useProjectsData)(), status = _c.status, data = _c.data;
     if (status === 'loading') {
         return react_1.default.createElement(react_1.default.Fragment, null,
             react_1.default.createElement(ProjectTabs_1.default, null),
@@ -64,13 +65,22 @@ var ProjectsPage = function () {
         }
         return null;
     };
-    if (data.length < 1) {
+    var projects = (0, Projects_1.transformProjects)(data);
+    if (projects.length < 1) {
         return react_1.default.createElement(react_1.default.Fragment, null,
             portals(),
             react_1.default.createElement(ProjectTabs_1.default, null),
             react_1.default.createElement(NoResultsAddItem_1.default, { icon: react_1.default.createElement(solid_1.ClipboardDocumentListIcon, null), headline: "No projects", content: "Would you like to create a project?", actionText: "Add New Project", actionUsesPlusIcon: true, actionButtonOnClick: function () { setAddProjectIsOpen(true); } }));
     }
-    var projects = (0, Projects_1.transformProjects)(data);
+    var FilterInput = react_1.default.createElement("div", { className: "mb-4" },
+        react_1.default.createElement("input", { type: "text", name: "filter", id: "filter", className: "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6", placeholder: "Filter results", value: filterText, onChange: function (e) {
+                setFilterText(e.target.value);
+            } }));
+    if (filterText !== '') {
+        projects = projects.filter(function (project) { return project.title.toLowerCase().indexOf(filterText.toLowerCase()) > -1
+            || project.slug.toLowerCase().indexOf(filterText.toLowerCase()) > -1
+            || project.description.toLowerCase().indexOf(filterText.toLowerCase()) > -1; });
+    }
     return react_1.default.createElement(react_1.default.Fragment, null,
         portals(),
         react_1.default.createElement(ProjectTabs_1.default, null),
@@ -81,46 +91,52 @@ var ProjectsPage = function () {
                     react_1.default.createElement("button", { type: "button", className: "inline-flex items-center block rounded-md bg-cyan-600 py-1.5 px-3 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600", onClick: function () { setAddProjectIsOpen(true); } },
                         react_1.default.createElement(solid_1.PlusIcon, { className: "-ml-1 mr-2 h-5 w-5", "aria-hidden": "true" }),
                         "Add New Project"))),
-            react_1.default.createElement("div", { className: "bg-white rounded-xl px-4" },
-                react_1.default.createElement("ul", { role: "list", className: "divide-y divide-gray-100" }, projects.map(function (project) { return (react_1.default.createElement("li", { key: project.id, className: "flex items-center justify-between gap-x-6 py-5" },
-                    react_1.default.createElement("div", { className: "min-w-0" },
-                        react_1.default.createElement("div", { className: "flex items-start gap-x-3" },
-                            react_1.default.createElement("p", { className: "text-sm font-semibold leading-6 text-gray-900" }, project.title),
-                            react_1.default.createElement("p", { className: classNames(statuses[project.status.toString()], 'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset') }, project.status.toString())),
-                        react_1.default.createElement("div", { className: "mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500" },
-                            react_1.default.createElement("p", { className: "whitespace-nowrap" }, project.slug),
-                            react_1.default.createElement("svg", { viewBox: "0 0 2 2", className: "h-0.5 w-0.5 fill-current" },
-                                react_1.default.createElement("circle", { cx: 1, cy: 1, r: 1 })),
-                            react_1.default.createElement("p", { className: "truncate" },
-                                "Created ",
-                                project.createdAtDate.toLocaleDateString()))),
-                    react_1.default.createElement("div", { className: "flex flex-none items-center gap-x-4" },
-                        react_1.default.createElement(react_router_dom_1.Link, { to: project.href, className: "block rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" },
-                            "View project",
-                            react_1.default.createElement("span", { className: "sr-only" },
-                                ", ",
-                                project.title)),
-                        react_1.default.createElement(react_2.Menu, { as: "div", className: "relative flex-none" },
-                            react_1.default.createElement(react_2.Menu.Button, { className: "-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900" },
-                                react_1.default.createElement("span", { className: "sr-only" }, "Open options"),
-                                react_1.default.createElement(solid_1.EllipsisVerticalIcon, { className: "h-5 w-5", "aria-hidden": "true" })),
-                            react_1.default.createElement(react_2.Transition, { as: react_1.Fragment, enter: "transition ease-out duration-100", enterFrom: "transform opacity-0 scale-95", enterTo: "transform opacity-100 scale-100", leave: "transition ease-in duration-75", leaveFrom: "transform opacity-100 scale-100", leaveTo: "transform opacity-0 scale-95" },
-                                react_1.default.createElement(react_2.Menu.Items, { className: "absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" },
-                                    react_1.default.createElement(react_2.Menu.Item, null, function (_a) {
-                                        var active = _a.active;
-                                        return (react_1.default.createElement("span", { className: classNames(active ? 'bg-gray-50' : '', 'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900') },
-                                            "Edit",
-                                            react_1.default.createElement("span", { className: "sr-only" },
-                                                ", ",
-                                                project.title)));
-                                    }),
-                                    react_1.default.createElement(react_2.Menu.Item, null, function (_a) {
-                                        var active = _a.active;
-                                        return (react_1.default.createElement("span", { className: classNames(active ? 'bg-gray-50' : '', 'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900') },
-                                            "Archive",
-                                            react_1.default.createElement("span", { className: "sr-only" },
-                                                ", ",
-                                                project.title)));
-                                    }))))))); })))));
+            FilterInput,
+            (function () {
+                if (projects.length < 1) {
+                    return react_1.default.createElement(NoResultsAddItem_1.default, { icon: react_1.default.createElement(solid_1.ClipboardDocumentListIcon, null), headline: "No projects match your filters" });
+                }
+                return react_1.default.createElement("div", { className: "bg-white rounded-xl px-4" },
+                    react_1.default.createElement("ul", { role: "list", className: "divide-y divide-gray-100" }, projects.map(function (project) { return (react_1.default.createElement("li", { key: project.id, className: "flex items-center justify-between gap-x-6 py-5" },
+                        react_1.default.createElement("div", { className: "min-w-0" },
+                            react_1.default.createElement("div", { className: "flex items-start gap-x-3" },
+                                react_1.default.createElement("p", { className: "text-sm font-semibold leading-6 text-gray-900" }, project.title),
+                                react_1.default.createElement("p", { className: classNames(statuses[project.status.toString()], 'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset') }, project.status.toString())),
+                            react_1.default.createElement("div", { className: "mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500" },
+                                react_1.default.createElement("p", { className: "whitespace-nowrap" }, project.slug),
+                                react_1.default.createElement("svg", { viewBox: "0 0 2 2", className: "h-0.5 w-0.5 fill-current" },
+                                    react_1.default.createElement("circle", { cx: 1, cy: 1, r: 1 })),
+                                react_1.default.createElement("p", { className: "truncate" },
+                                    "Created ",
+                                    project.createdAtDate.toLocaleDateString()))),
+                        react_1.default.createElement("div", { className: "flex flex-none items-center gap-x-4" },
+                            react_1.default.createElement(react_router_dom_1.Link, { to: project.href, className: "block rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" },
+                                "View project",
+                                react_1.default.createElement("span", { className: "sr-only" },
+                                    ", ",
+                                    project.title)),
+                            react_1.default.createElement(react_2.Menu, { as: "div", className: "relative flex-none" },
+                                react_1.default.createElement(react_2.Menu.Button, { className: "-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900" },
+                                    react_1.default.createElement("span", { className: "sr-only" }, "Open options"),
+                                    react_1.default.createElement(solid_1.EllipsisVerticalIcon, { className: "h-5 w-5", "aria-hidden": "true" })),
+                                react_1.default.createElement(react_2.Transition, { as: react_1.Fragment, enter: "transition ease-out duration-100", enterFrom: "transform opacity-0 scale-95", enterTo: "transform opacity-100 scale-100", leave: "transition ease-in duration-75", leaveFrom: "transform opacity-100 scale-100", leaveTo: "transform opacity-0 scale-95" },
+                                    react_1.default.createElement(react_2.Menu.Items, { className: "absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" },
+                                        react_1.default.createElement(react_2.Menu.Item, null, function (_a) {
+                                            var active = _a.active;
+                                            return (react_1.default.createElement("span", { className: classNames(active ? 'bg-gray-50' : '', 'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900') },
+                                                "Edit",
+                                                react_1.default.createElement("span", { className: "sr-only" },
+                                                    ", ",
+                                                    project.title)));
+                                        }),
+                                        react_1.default.createElement(react_2.Menu.Item, null, function (_a) {
+                                            var active = _a.active;
+                                            return (react_1.default.createElement("span", { className: classNames(active ? 'bg-gray-50' : '', 'cursor-pointer block px-3 py-1 text-sm leading-6 text-gray-900') },
+                                                "Archive",
+                                                react_1.default.createElement("span", { className: "sr-only" },
+                                                    ", ",
+                                                    project.title)));
+                                        }))))))); })));
+            })()));
 };
 exports.default = ProjectsPage;
