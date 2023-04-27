942c76c2c0664c0823dab999f6c1598bf4a91049
TJ Draper <tj@buzzingpixel.com>
2023-04-27 13:43:24 -0500

    Add project details page heading

--- assets/Projects/ProjectDetails/ProjectDetailsPage.tsx
+++ assets/Projects/ProjectDetails/ProjectDetailsPage.tsx
@@ -5,6 +5,15 @@ import PartialPageLoading from '../../PartialPageLoading';
 import { transformProject } from '../Projects';
 import { useBreadcrumbs, useHidePageTitle, usePageTitle } from '../../RouteContext/RouteContext';
 
+const statuses = {
+    Active: 'text-green-700 bg-green-50 ring-green-600/20',
+    Archived: 'text-yellow-800 bg-yellow-50 ring-yellow-600/20',
+};
+
+function classNames (...classes) {
+    return classes.filter(Boolean).join(' ');
+}
+
 const ProjectDetailsPage = () => {
     const { slug } = useParams();
 
@@ -58,10 +67,61 @@ const ProjectDetailsPage = () => {
         setIsArchive(true);
     }
 
-    // console.log(project);
-
     return <>
-        ProjectDetailsPage
+        <div className="border-b border-gray-200 pb-4">
+            <div className="md:flex md:items-center md:justify-between md:space-x-5">
+                <div className="flex items-start space-x-5">
+                    {/*
+                      Use vertical padding to simulate center alignment when both lines of text are one line,
+                      but preserve the same layout if the text wraps without making the image jump around.
+                    */}
+                    <div className="pt-1.5">
+                        <h1 className="text-2xl font-bold text-gray-900">
+                            {project.title}
+                            <svg viewBox="0 0 2 2" className="h-6 w-10 fill-current inline px-4">
+                                <circle cx={1} cy={1} r={1}/>
+                            </svg>
+                            <code className="font-light">
+                                {project.slug}
+                            </code>
+                            <code className="font-light">
+                                {project.slug}
+                            </code>
+                            <svg viewBox="0 0 2 2" className="h-6 w-10 fill-current inline px-4">
+                                <circle cx={1} cy={1} r={1}/>
+                            </svg>
+                            <div className="inline-block pb-1 align-bottom">
+                                <p
+                                    className={classNames(
+                                        statuses[project.status.toString()],
+                                        'rounded-md whitespace-nowrap px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset inline-block',
+                                    )}
+                                >
+                                    {project.status.toString()}
+                                </p>
+                            </div>
+                        </h1>
+                        {(() => {
+                            if (!project.description) {
+                                return null;
+                            }
+
+                            return <p className="text-sm font-medium text-gray-500">
+                                {project.description}
+                            </p>;
+                        })()}
+                    </div>
+                </div>
+                <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
+                    <button
+                        type="button"
+                        className="inline-flex items-center justify-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+                    >
+                        Edit Project
+                    </button>
+                </div>
+            </div>
+        </div>
     </>;
 };
 
--- dist/Projects/ProjectDetails/ProjectDetailsPage.js
+++ dist/Projects/ProjectDetails/ProjectDetailsPage.js
@@ -32,6 +32,17 @@ var ProjectDetailsData_1 = require("./ProjectDetailsData");
 var PartialPageLoading_1 = __importDefault(require("../../PartialPageLoading"));
 var Projects_1 = require("../Projects");
 var RouteContext_1 = require("../../RouteContext/RouteContext");
+var statuses = {
+    Active: 'text-green-700 bg-green-50 ring-green-600/20',
+    Archived: 'text-yellow-800 bg-yellow-50 ring-yellow-600/20',
+};
+function classNames() {
+    var classes = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        classes[_i] = arguments[_i];
+    }
+    return classes.filter(Boolean).join(' ');
+}
 var ProjectDetailsPage = function () {
     var slug = (0, react_router_dom_1.useParams)().slug;
     (0, RouteContext_1.useHidePageTitle)(true);
@@ -61,7 +72,28 @@ var ProjectDetailsPage = function () {
     if (isArchive !== !project.isActive) {
         setIsArchive(true);
     }
-    // console.log(project);
-    return react_1.default.createElement(react_1.default.Fragment, null, "ProjectDetailsPage");
+    return react_1.default.createElement(react_1.default.Fragment, null,
+        react_1.default.createElement("div", { className: "border-b border-gray-200 pb-4" },
+            react_1.default.createElement("div", { className: "md:flex md:items-center md:justify-between md:space-x-5" },
+                react_1.default.createElement("div", { className: "flex items-start space-x-5" },
+                    react_1.default.createElement("div", { className: "pt-1.5" },
+                        react_1.default.createElement("h1", { className: "text-2xl font-bold text-gray-900" },
+                            project.title,
+                            react_1.default.createElement("svg", { viewBox: "0 0 2 2", className: "h-6 w-10 fill-current inline px-4" },
+                                react_1.default.createElement("circle", { cx: 1, cy: 1, r: 1 })),
+                            react_1.default.createElement("code", { className: "font-light" }, project.slug),
+                            react_1.default.createElement("code", { className: "font-light" }, project.slug),
+                            react_1.default.createElement("svg", { viewBox: "0 0 2 2", className: "h-6 w-10 fill-current inline px-4" },
+                                react_1.default.createElement("circle", { cx: 1, cy: 1, r: 1 })),
+                            react_1.default.createElement("div", { className: "inline-block pb-1 align-bottom" },
+                                react_1.default.createElement("p", { className: classNames(statuses[project.status.toString()], 'rounded-md whitespace-nowrap px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset inline-block') }, project.status.toString()))),
+                        (function () {
+                            if (!project.description) {
+                                return null;
+                            }
+                            return react_1.default.createElement("p", { className: "text-sm font-medium text-gray-500" }, project.description);
+                        })())),
+                react_1.default.createElement("div", { className: "mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3" },
+                    react_1.default.createElement("button", { type: "button", className: "inline-flex items-center justify-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600" }, "Edit Project")))));
 };
 exports.default = ProjectDetailsPage;
